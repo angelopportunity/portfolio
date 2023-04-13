a9e0da68f0821b337c2861e7b459f67b5d3de4b9
angelopportunity <111150915+angelopportunity@users.noreply.github.com>
2023-04-13 16:53:39 -0700

    unstable build, saving in case I lose it, adding cards for mobile etc.

---
+++ src/Components/PortfolioCard.jsx
@@ -0,0 +1,17 @@
+export default function PortfolioCard(props) {
+
+    return (
+        <div className="portfolio--card--mobile">
+            <div className="card--title--mobile">
+                EVOLVING SHIPS
+            </div>
+            <div className="card--copy--mobile">
+                This is the project that...
+            </div>
+            <div className="sketch--container--mobile">
+                <div className="pic--mobile">static pic with link</div>
+                <div className="pic--caption--mobile">caption text</div>
+            </div>
+        </div>
+    )
+}
